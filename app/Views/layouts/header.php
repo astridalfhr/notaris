68033d182060ec4b33d61570ce2ee16512f9c11c
astridalfhr <astridalifiah@gmail.com>
2025-09-08 02:26:10 +0700

    bismillah final lagi dehhhh

--- app/Views/layouts/header.php
+++ app/Views/layouts/header.php
@@ -6,7 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?= $title ?? 'Notaris' ?></title>
   <script src="https://cdn.tailwindcss.com"></script>
-  <link rel="stylesheet" href="<?= base_url('assets/css/style.css') ?>">
+  <link rel="stylesheet" href="<?= rtrim(base_url(), '/') . '/public/assets/css/style.css' ?>">
   <script src="https://accounts.google.com/gsi/client" async defer></script>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
   <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
